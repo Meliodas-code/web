6506bfca8bbf350e9f54b46da79727c122c29892
Meliodas-code <delarosasaul2008@gmail.com>
2026-05-03 12:37:41 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // ESTA LÍNEA ES LA QUE QUITA EL ERROR 404
+  // Esto arregla los errores 404
   base: "/web/", 
-  plugins: [react()],
+  root: ".",
+  publicDir: "public",
   build: {
     outDir: "dist",
+    emptyOutDir: true,
   },
 });
