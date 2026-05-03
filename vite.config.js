e2cf850ed084f7f2f4bbe4411e0440948b319d6e
Meliodas-code <delarosasaul2008@gmail.com>
2026-05-03 02:13:56 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,17 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  const base = env.VITE_BASE_PATH || "/";
-  return {
-    base,
-    root: ".",
-    publicDir: "public",
-    server: {
-      port: 5173,
-      open: true,
-    },
-    build: {
-      outDir: "dist",
-      sourcemap: false,
-      emptyOutDir: true,
-    },
-  };
+export default defineConfig({
+  // Forzamos la ruta al nombre de tu repo
+  base: "/web/", 
+  root: ".",
+  publicDir: "public",
+  server: {
+    port: 5173,
+    open: true,
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    emptyOutDir: true,
+  },
 });
