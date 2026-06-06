df2dbbe670d513f814d1df396b70115e31a54951
Meliodas-code <delarosasaul2008@gmail.com>
2026-06-06 20:08:33 +0200

    Create index.js

---
+++ src/visualMatch/index.js
@@ -0,0 +1,10 @@
+export {
+  VECTOR_SIZE,
+  MATCH_THRESHOLD,
+  loadImage,
+  similarityToConfidence,
+  compareFeatureSets,
+} from "./features.js";
+export { warmUnitTemplates, clearTemplateCache } from "./templates.js";
+export { estimateCardCount } from "./cardDetection.js";
+export { compareImageWithUnits } from "./compareImageWithUnits.js";
